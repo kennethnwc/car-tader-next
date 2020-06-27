9cd50f17a2f673ed5fbf190cff6412c351db4562
Kenneth NG <nwc27101994@yahoo.com.hk>
2020-06-27 16:37:51 +0800

    Added Faq content

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,3 +1,25 @@
-export default function Faq (){
-  return <div>FAQ</div>
+import { GetStaticProps } from "next";
+import { openDB } from "../openDB";
+import { FaqModel } from "../../api/Faq";
+
+type Props = {
+  faq: FaqModel[];
+};
+
+export default function Faq({ faq }: Props) {
+  return (
+    <div>
+      {faq.map((f) => (
+        <div>
+          {f.question} | {f.answer}
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const db = await openDB();
+  const faq = await db.all("SELECT * FROM FAQ ORDER BY createDate DESC");
+  return { props: { faq } };
+};
